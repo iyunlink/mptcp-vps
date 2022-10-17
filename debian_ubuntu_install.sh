ae8dfe76c6cb9f75bdcb33998d70fdbb2d870f13
link4all <279357174@qq.com>
2022-10-17 15:46:54 +0800

    clean apt before install

--- debian_ubuntu_install.sh
+++ debian_ubuntu_install.sh
@@ -54,7 +54,13 @@ if [ "$?" -ne 0 ]; then
 	exit 1
 fi
 
-
+#clean apt
+mv /var/lib/dpkg/info  /var/lib/dpkg/info_bak
+mkdir /var/lib/dpkg/info
+apt-get update && apt-get -f install
+mv /var/lib/dpkg/info/* /var/lib/dpkg/info_bak/
+rm -rf /var/lib/dpkg/info
+mv /var/lib/dpkg/info_bak /var/lib/dpkg/info
 
 echo "Remove lock and update packages list..."
 rm -f /var/lib/dpkg/lock
